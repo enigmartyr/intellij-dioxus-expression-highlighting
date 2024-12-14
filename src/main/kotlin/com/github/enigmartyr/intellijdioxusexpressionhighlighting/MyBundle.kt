30585d419914b6661f30f9eef8181db9d3d54b76
GitHub Action <action@github.com>
2024-12-14 17:10:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/enigmartyr/intellijdioxusexpressionhighlighting/MyBundle.kt
+++ src/main/kotlin/com/github/enigmartyr/intellijdioxusexpressionhighlighting/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.enigmartyr.intellijdioxusexpressionhighlighting
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
